2772debc753f884f392788a0c220791f935ebf41
Andrzej Kałowski <a.m.kalowski@gmail.com>
2018-06-02 17:27:15 +0200

    Initial commit

---
+++ src/app/figures/calculation-result/calculation-shape/empty-shape/empty-shape.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-shape',
+  templateUrl: './empty-shape.component.html',
+  styleUrls: ['./empty-shape.component.css']
+})
+
+export class EmptyShapeComponent {}
